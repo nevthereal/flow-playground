a2cb5da48ffbe8fe2397eee08de2fbfc1ea49ca9
Neville Brem <contact@nevillebrem.com>
2025-02-18 20:06:30 +0100

    tried implementing node addition feat

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '@xyflow/svelte/dist/style.css';
 	import '../app.css';
+	import { SvelteFlowProvider } from '@xyflow/svelte';
 	let { children } = $props();
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,41 @@
 <script lang="ts">
-	import { SvelteFlow, Background, Controls, ControlButton } from '@xyflow/svelte';
+	import { SvelteFlow, Background, Controls, ControlButton, useNodes } from '@xyflow/svelte';
 	import { PersistedState } from 'runed';
 	import type { Node, Edge } from '@xyflow/svelte';
+	import { generateCode } from '@nevthereal/random-utils';
 
-	const nodes = new PersistedState<Node[]>('nodes', [
+	let nodes = $state.raw<Node[]>([
 		{
-			id: '1', // required and needs to be a string
-			position: { x: 0, y: 0 }, // required
-			data: { label: 'hey' } // required
+			id: '1',
+			position: { x: 0, y: 0 },
+			data: { label: 'This' }
 		},
 		{
 			id: '2',
 			position: { x: 100, y: 100 },
-			data: { label: 'world' }
+			data: { label: 'Is cool' }
 		}
 	]);
-	const edges = new PersistedState<Edge[]>('edges', [{ id: '1-2', source: '1', target: '2' }]);
+
+	let edges = $state.raw<Edge[]>([{ id: '1-2', source: '1', target: '2' }]);
+
+	function addNode() {
+		nodes.push({ id: generateCode(5), position: { x: 0, y: 0 }, data: { label: 'Hi' } });
+	}
 </script>
 
-<main class="h-screen w-screen">
-	<SvelteFlow bind:nodes={nodes.current} bind:edges={edges.current} fitView={true}>
+<h1 class="my-4 text-center text-4xl font-bold">This is Svelte-Flow</h1>
+<main class="mx-auto h-150 w-200">
+	<SvelteFlow
+		snapGrid={[25, 25]}
+		fitViewOptions={{ nodes: nodes, minZoom: 50 }}
+		bind:nodes
+		bind:edges
+	>
 		<Background />
-		<Controls showLock={false}>
-			<ControlButton onclick={() => console.log('⚡️')}>⚡️</ControlButton>
-		</Controls>
+		<Controls showLock={false} />
 	</SvelteFlow>
+	<button onclick={addNode} class="rounded-xl bg-orange-500 p-2 font-bold text-white"
+		>Add new Node</button
+	>
 </main>
